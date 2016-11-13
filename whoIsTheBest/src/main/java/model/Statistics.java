a318e46caeef5ed0aa522399fcf9fd9122dc97dc
Miau <Miau@Miau-Komputer>
2016-11-13 15:34:10 +0100

    Implemented database classes

---
+++ whoIsTheBest/src/main/java/model/Statistics.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Statistics {
+	private int kills;
+	private int deaths;
+	private double kdRatio;
+	
+	
+	public int getKills() {
+		return kills;
+	}
+	public void setKills(int kills) {
+		this.kills = kills;
+	}
+	public int getDeaths() {
+		return deaths;
+	}
+	public void setDeaths(int deaths) {
+		this.deaths = deaths;
+	}
+	public double getKdRatio() {
+		return kdRatio;
+	}
+	public void setKdRatio(double kdRatio) {
+		this.kdRatio = kdRatio;
+	}
+	
+	
+}
